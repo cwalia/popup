eb7d0f775e84e8eb9fcffa056e7b8773441ffca0
Chiranshu Walia <waliachiranshu77@gmail.com>
2023-05-19 00:18:59 +0530

    more of css

--- src/components/SideBarPanel.vue
+++ src/components/SideBarPanel.vue
@@ -1,16 +1,45 @@
 <template>
   <div class="side-bar-panel">
-    hey SideBarPanel
+    <div class="side-bar-panel__header">
+      Action
+    </div>
+    <div class="side-bar-panel__assActions">
+      Assigned actions
+      <div class="side-bar-panel__assActions__sub-head">The selected actions will run in the background when the user journey gets to this element</div>
+      <button>Add actions</button>
+    </div>
+    
+    <div class="side-bar-panel__action-area">
+      <!-- <b-input-group class="mt-3">
+        <template #prepend>
+          <b-input-group-text><strong class="text-danger">!</strong></b-input-group-text>
+        </template>
+        <b-form-input></b-form-input>
+      </b-input-group> -->
+      <div class="input-container">
+        <i class="fa fa-user"></i>
+        <input type="text" placeholder="Username">
+      </div>
+      <action-button :buttonData="actionButtonList[0]"></action-button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import {Component, Vue, Prop} from 'vue-property-decorator';
-
+import ActionButton from './common/ActionButton.vue';
 @Component({
   components: {
+    ActionButton
   },
 })
 export default class SideBarPanel extends Vue {
+  actionButtonList = [
+    {
+      iconName : 'hey',
+      text : 'Tag customer',
+      checkboxValue : false,
+    }
+  ]
 }
 </script>
---
+++ src/components/common/ActionButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="action-button">
+    <div class="action-button__button-bar">
+        <div class="action-button__icon-text">
+            {{buttonData.text}}
+        </div>
+        <b-form-checkbox v-model="buttonData.checkboxValue"></b-form-checkbox>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue, Prop} from 'vue-property-decorator';
+
+@Component({
+  components: {
+  },
+})
+export default class ActionButton extends Vue {
+    @Prop() private buttonData!: any;
+
+}
+</script>
